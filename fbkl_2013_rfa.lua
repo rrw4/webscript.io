c4c5e781c32fafd56310f3ca57199e8481e73075
Roger Wang <r.rj.wang@gmail.com>
2013-09-16 22:46:24 -0500

    Rename to .lua and clean up formatting

--- fbkl_2013_rfa.lua
+++ fbkl_2013_rfa.lua
@@ -15,13 +15,17 @@ players = { {"Paul George", "SG", "IND", 8, "Lam"},
 						{"Xavier Henry", "SG", "LAL", 5, "Mike"},
 						{"Greg Monroe", "C", "DET", 12, "Will"},
 						{"Timofey Mozgov", "C", "DEN", 4, "Peter/Mike"},
-						{"Al-Farouq Aminu", "SF", "NOH", 6, "Cho" }}
+            {"Al-Farouq Aminu", "SF", "NOH", 6, "Cho" }}
 
 email.send {
-	server=server, username=username, password=password,
-	from='FBKL Auction Bot <r.rj.wang@gmail.com>',
-	to='r.rj.wang@gmail.com',
-	subject="2013 Veteran Auction RFA: " .. players[rfa_count][2] .. " " .. players[rfa_count][1] .. " " .. players[rfa_count][3] .. ", minimum bid $" .. players[rfa_count][4] .. " (owner: " .. players[rfa_count][5] .. ")",
-	text="2013 Veteran Auction RFA: " .. players[rfa_count][2] .. " " .. players[rfa_count][1] .. " " .. players[rfa_count][3] .. ", minimum bid $" .. players[rfa_count][4] .. " (owner: " .. players[rfa_count][5] .. ")"
+	server = server,
+  username = username,
+  password = password,
+	from = 'FBKL Auction Bot <r.rj.wang@gmail.com>',
+	to = 'r.rj.wang@gmail.com',
+	subject = "2013 Veteran Auction RFA: " .. players[rfa_count][2] .. " " .. players[rfa_count][1] .. " " .. players[rfa_count][3] ..
+    ", minimum bid $" .. players[rfa_count][4] .. " (owner: " .. players[rfa_count][5] .. ")",
+	text = "2013 Veteran Auction RFA: " .. players[rfa_count][2] .. " " .. players[rfa_count][1] .. " " .. players[rfa_count][3] ..
+    ", minimum bid $" .. players[rfa_count][4] .. " (owner: " .. players[rfa_count][5] .. ")"
 }
 return "Email sent. Thanks!"
